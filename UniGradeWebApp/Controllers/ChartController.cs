6b3f53ef83d53d62d4f5d927a52844c50c1a394d
Taras Livisky <astralivinsky@knu.ua>
2023-05-15 12:36:12 +0300

    Optimized chart

--- UniGradeWebApp/Controllers/ChartController.cs
+++ UniGradeWebApp/Controllers/ChartController.cs
@@ -16,54 +16,53 @@ namespace UniGradeWebApp.Controllers
         [HttpGet("JsonGroupGradeData")]
         public JsonResult JsonGroupGradeData(int CathId)
         {
-            var groupNameById = new Dictionary<int, string>();
-            foreach (var g in _context.Groups)
-            {
-                if (g.GrpCath == CathId)
-                {
-                    groupNameById.Add(g.GrpId, g.GrpName);
-                }
-            }
-            var students = new Dictionary<int, (int, double?)>();
-            foreach (var s in _context.Students.Include(s =>s.Grades))
-            {
-                if (groupNameById.ContainsKey(s.StnGrp))
-                {
-                    double? avg = s.Grades.Average(g => g.GrdResult);
-                    students.Add(s.StnId, (s.StnGrp, avg));
-                }
-            }
-            var groupGrades = new Dictionary<int, List<double>>();
-            foreach (var s in students)
-            {
-                if (s.Value.Item2 is not null)
-                {
-                    groupGrades.TryAdd(s.Value.Item1, new List<double>());
-                    groupGrades[s.Value.Item1].Add((double)s.Value.Item2);
-                }
-            }
+            //List з назвами груп і їх середніми балами, для груп однієї кафедри (у якої заданий ID) 
+            var grade_data = (from g in _context.Groups
+                         join st in
+                         (from s in _context.Students
+                          join gr in
+                          (from gr in _context.Grades
+                           group gr by gr.GrdStn into grAvg
+                           select new
+                           {
+                               GrdStn = grAvg.Key,
+                               AvgPerStudent = grAvg.Average(gr => gr.GrdResult)
+                           })
+                          on s.StnId equals gr.GrdStn
+                          group gr by s.StnGrp into stAvg
+                          select new
+                          {
+                              StnGrp = stAvg.Key,
+                              AvgPerGroup = stAvg.Average(st => st.AvgPerStudent)
+                          })
+                         on g.GrpId equals st.StnGrp
+                         where g.GrpCath == 2
+                         select new
+                         {
+                             GrpName = g.GrpName,
+                             AvgPerGroup = st.AvgPerGroup
+                         }).ToList();
             var result = new List<object> { new[] { "Група", "Середній бал" } };
-            foreach (var g in groupGrades)
+            foreach (var g in grade_data)
             {
-                result.Add(new object[] { groupNameById[g.Key], g.Value.Average() });
+                result.Add(new object[] { g.GrpName, g.AvgPerGroup });
             }
             return new JsonResult(result);
         }
         [HttpGet("JsonStudentGradeData")]
         public JsonResult JsonStudentGradeData(int StnId)
         {
-            var grds = new List<object> { new[] { "Предмет", "Бал" } };
-            foreach (var s in _context.Students.Include(s => s.Grades).ThenInclude(g => g.GrdSbjNavigation))
+            //List з оцінки даного студента після назв відповідних предметів
+            var result = new List<object> { new[] { "Предмет", "Бал" } };
+            var grades = (from g in _context.Grades
+                         join s in _context.Subjects on g.GrdSbj equals s.SbjId
+                         where g.GrdStn == StnId
+                         select new { SbjName = s.SbjName, GrdResult = g.GrdResult }).ToList();
+            foreach (var g in grades)
             {
-                if (s.StnId == StnId)
-                {
-                    foreach (var g in s.Grades)
-                    {
-                        grds.Add(new object[] { g.GrdSbjNavigation.SbjName, (double)g.GrdResult });
-                    }
-                }
+                result.Add(new object[] { g.SbjName, g.GrdResult });
             }
-            return new JsonResult(grds);
+            return new JsonResult(result);
         }
     }
 }
